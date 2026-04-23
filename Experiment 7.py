1b90b31bea29d90c1671dd62fd2c41d8f9009929
Sumit Kirwale <sumitkirwale01@gmail.com>
2026-04-23 16:25:24 +1100

    Add files via upload

---
+++ Experiment 7.py
@@ -0,0 +1,37 @@
+#Tuble
+B2a=(10,20,30,40,50,60,70,80,90,100)
+B2b=(100,200,300,400,500,600,700,800,900,1000)
+print(B2a)
+print(B2b)
+
+#Ccatination
+A=B2a+B2b
+print(A)
+
+#Type of Tuble
+print(type(A))
+
+#Slicing Operation
+print(A[5:9])
+print(A[:9])
+print(A[5:])
+
+#length of Tuble
+print(len(A))
+
+#Repeatation
+print((A)*5)
+
+#Minimum value and m aximum value
+print(min(A))
+print(max(A))
+
+#Converting Tuple into list
+print(list(A))
+
+#Convertion list into Tuble
+print(tuple(A))
+
+#membarship Operation
+print(25 in A)
+print(100 in A)
