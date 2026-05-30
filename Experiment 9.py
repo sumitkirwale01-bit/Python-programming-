789d8e385625c2df8065049bdcec1032b926ef8c
Sumit Kirwale <sumitkirwale01@gmail.com>
2026-05-30 09:45:48 +0530

    Add files via upload

---
+++ Experiment 9.py
@@ -0,0 +1,21 @@
+#Dictionary
+a={23:"om", 24:"akshaf", 25:"Nil", 26:"pranalt", "CT":"D D Nale"}
+print(a)
+
+#Checking Datatype
+print(type(a))
+
+#checking legth of Dict
+print(len(a))
+
+#Accessing keys
+print(a.keys())
+
+#Accessing values
+print(a.values())
+
+#Accessing the value from key
+print(a['CT'])
+      
+#converting dict in to List or tuple
+print(tuple(a))
